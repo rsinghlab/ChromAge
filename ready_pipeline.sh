f2a57539e9363f189f087fcf3305b2210ec2456b
Muhammad Haider Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-10-02 21:27:47 -0400

    updated bash file to also keep qc reports

--- ready_pipeline.sh
+++ ready_pipeline.sh
@@ -38,7 +38,7 @@ cd /gpfs/data/rsingh47/masif/caper_output/chip/$d
 croo metadata.json
 cd "$part1$d$part2"
 pwd
-cp "$part1$d$part3" /gpfs/data/rsingh47/masif/caper_output/qcReports/$d
+mv "$part1$d$part3" /gpfs/data/rsingh47/masif/caper_output/qcReports/$d
 for e in *.bigwig ; do
 echo $e
 mv $e /gpfs/data/rsingh47/masif/caper_output/bigWigs
