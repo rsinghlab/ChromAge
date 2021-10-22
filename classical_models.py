6401f62d9ce7c1c469da8e251507e4cfc0f82865
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-10-21 23:31:37 -0400

    Update classical_models.py

--- classical_models.py
+++ classical_models.py
@@ -509,7 +509,7 @@ param_grid = {
     'neural_network__coeff':[0.005, 0.05, 0.01],
 }
 
-pipeline = Pipeline(steps= [('imputer', KNNImputer()), StandardScaler(), ('neural_network', neural_network)])
+pipeline = Pipeline(steps = [('imputer', KNNImputer()), ('scaler', StandardScaler()), ('neural_network', neural_network)])
 
 # (steps = [('imputer', KNNImputer()), neural_network])
 
