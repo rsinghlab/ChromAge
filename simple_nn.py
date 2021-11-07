54b15b7f907d2158dc9b72bc8d2b169c40671189
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-11-06 21:00:38 -0400

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -454,7 +454,7 @@ class MyModel(Model):
 histone_data_object = pickle.load(open('/users/masif/data/masif/ChromAge/encode_histone_data/human/tissue/H3K4me3/processed_data/H3K4me3_mean_bins.pkl', 'rb'))
 
 metadata = pd.read_pickle('/users/masif/data/masif/ChromAge/encode_histone_data/human/tissue/metadata_summary.pkl') 
-metadata = filter_metadata(metadata, biological_replicates = True)
+metadata = filter_metadata(metadata, biological_replicates = False)
 
 X_train, X_test, y_train, y_test = split_data(metadata, histone_data_object)
 X_train, X_test, y_train, y_test = np.asarray(X_train), np.asarray(X_test), np.asarray(y_train), np.asarray(y_test)
