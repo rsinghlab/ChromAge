86ba1068f3889793457338e8d6209b9c91d5dfb8
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-11-24 12:35:59 -0500

    Update extract_output.sh

--- extract_output.sh
+++ extract_output.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-cd /gpfs/data/rsingh47/masif/caper_output/chip
+# cd /gpfs/data/rsingh47/masif/caper_output/chip
 
-part1="/gpfs/data/rsingh47/masif/caper_output/chip/"
-part2="signal/rep1/"
-part3="qc"
+# part1="/gpfs/data/rsingh47/masif/caper_output/chip/"
+# part2="signal/rep1/"
+# part3="qc"
 
-conda activate encode-chip-seq-pipeline
+# conda activate encode-chip-seq-pipeline
 
+# for d in */ ; do
+# echo $d
+# cd /gpfs/data/rsingh47/masif/caper_output/chip/$d
+# croo metadata.json
+# mv "$part1$d$part3" /gpfs/data/rsingh47/masif/caper_output/qcReports/$d
+# cd "$part1$d$part2"
+# for e in *.bigwig ; do
+# echo $e
+# mv $e /gpfs/data/rsingh47/masif/caper_output/bigWigs
+# done
+# rm -r /gpfs/data/rsingh47/masif/caper_output/chip/$d
+# done
+
+cd /gpfs/data/rsingh47/masif/ChromAge/chip
 for d in */ ; do
 echo $d
-cd /gpfs/data/rsingh47/masif/caper_output/chip/$d
-croo metadata.json
-mv "$part1$d$part3" /gpfs/data/rsingh47/masif/caper_output/qcReports/$d
-cd "$part1$d$part2"
-for e in *.bigwig ; do
-echo $e
-mv $e /gpfs/data/rsingh47/masif/caper_output/bigWigs
-done
-rm -r /gpfs/data/rsingh47/masif/caper_output/chip/$d
+rm -r /gpfs/data/rsingh47/masif/ChromAge/chip/$d
 done
 
-cd /gpfs/data/rsingh47/masif/data/masif/ChromAge/chip
+cd /gpfs/data/rsingh47/masif/ChromAge/cromwell-workflow-logs
 for d in */ ; do
 echo $d
-rm -r /gpfs/data/rsingh47/masif/data/masif/ChromAge/chip/$d
+rm -f /gpfs/data/rsingh47/masif/ChromAge/cromwell-workflow-logs/$d
 done
 
-cd /gpfs/data/rsingh47/masif/data/masif/ChromAge/cromwell-workflow-logs
+cd /gpfs/data/rsingh47/masif/caper_output/qcReports
 for d in */ ; do
 echo $d
-rm -f /gpfs/data/rsingh47/masif/data/masif/ChromAge/cromwell-workflow-logs/$d
+rm -r /gpfs/data/rsingh47/masif/caper_output/qcReports/$d
 done
 
