191004f5943c5d01fdb4534e92f433df4aa34cc5
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-12-20 07:39:48 -0500

    Update extract_output.sh

--- extract_output.sh
+++ extract_output.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd /gpfs/data/rsingh47/masif/caper_output/chip
+cd /gpfs/data/rsingh47/masif/ChromAge/chip
 
-part1="/gpfs/data/rsingh47/masif/caper_output/chip/"
-part2="signal/rep1/"
-part3="qc"
+part1="/gpfs/data/rsingh47/masif/ChromAge/chip/"
+part2="call-macs2_signal_track/shard-0/execution"
+part3="call-qc_report"
 
 conda activate encode-chip-seq-pipeline
 
 for d in */ ; do
 echo $d
-cd /gpfs/data/rsingh47/masif/caper_output/chip/$d
+cd /gpfs/data/rsingh47/masif/ChromAge/chip/$d
 croo metadata.json
 mv "$part1$d$part3" /gpfs/data/rsingh47/masif/caper_output/qcReports/$d
 cd "$part1$d$part2"
@@ -17,13 +17,13 @@ for e in *.bigwig ; do
 echo $e
 mv $e /gpfs/data/rsingh47/masif/caper_output/bigWigs
 done
-rm -r /gpfs/data/rsingh47/masif/caper_output/chip/$d
+rm -r /gpfs/data/rsingh47/masif/ChromAge/chip/$d
 done
 
-cd /gpfs/data/rsingh47/masif/ChromAge/chip
+cd /gpfs/data/rsingh47/masif/caper_output/chip
 for d in */ ; do
 echo $d
-rm -r /gpfs/data/rsingh47/masif/ChromAge/chip/$d
+rm -r /gpfs/data/rsingh47/masif/caper_output/chip/$d
 done
 
 cd /gpfs/data/rsingh47/masif/ChromAge/cromwell-workflow-logs
