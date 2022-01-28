d3bb96121dadd07de49a28f544419ef7562999da
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-01-27 20:12:08 -0500

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -371,25 +371,21 @@ class AutoEncoder(tf.keras.Model):
         self.batch_size = 32
         self.loss = tf.keras.losses.MeanSquaredError()
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-        self.latent_size = 150
+        self.latent_size = 1500
         self.hidden_dim = 6000
         self.encoder = Sequential([
             Dense(self.hidden_dim, activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
+            Dropout(0.15),
             Dense(int(self.hidden_dim/2), activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
-            Dense(int(self.hidden_dim/4), activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
+            Dropout(0.15),
             Dense(self.latent_size, activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
+            Dropout(0.15),
         ])
         self.decoder = Sequential([
-            Dense(int(self.hidden_dim/4), activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
             Dense(int(self.hidden_dim/2), activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
+            Dropout(0.15),
             Dense(self.hidden_dim, activation='selu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-            Dropout(0.1),
+            Dropout(0.15),
             Dense(30321, activation=None)
         ])
     
