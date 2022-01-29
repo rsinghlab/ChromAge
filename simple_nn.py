4113a20df14df6bc929840530e6daf9912fef6c8
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-01-29 01:23:25 -0500

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -374,7 +374,7 @@ class AutoEncoder(tf.keras.Model):
         self.latent_size = 1500
         self.hidden_dim = 6000
         self.dropout_rate = 0.3
-        self.coeff = 0.01
+        self.coeff = 0.1
         self.encoder = Sequential([
             Dense(self.hidden_dim, activation='selu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
             Dropout(self.dropout_rate),
