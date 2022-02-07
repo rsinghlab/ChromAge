f74d3f9e194fdd647cc0fcad010a226d8e928611
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-02-07 18:17:13 -0500

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -386,16 +386,16 @@ class AutoEncoder(tf.keras.Model):
         self.coeff = coeff #0.01
         self.encoder = Sequential([
             GaussianNoise(gaussian_noise),
-            Dense(self.hidden_dim, activation='selu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
+            Dense(self.hidden_dim, activation='relu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
             Dropout(self.dropout_rate),
-            Dense(self.hidden_dim/2, activation='selu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
+            Dense(self.hidden_dim/2, activation='relu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
             Dropout(self.dropout_rate),
-            Dense(self.latent_size, activation='selu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
+            Dense(self.latent_size, activation='relu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
         ])
         self.decoder = Sequential([
-            Dense(int(self.hidden_dim/2), activation='selu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
+            Dense(int(self.hidden_dim/2), activation='relu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
             Dropout(self.dropout_rate),
-            Dense(self.hidden_dim, activation='selu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
+            Dense(self.hidden_dim, activation='relu', activity_regularizer=tf.keras.regularizers.l1_l2(self.coeff, self.coeff)),
             Dropout(self.dropout_rate),
             Dense(30321, activation=None)
         ])
@@ -551,7 +551,7 @@ def main(histone_data_object, histone_mark_str, process = False):
 
 if __name__ == '__main__':
     H3K4me3_data_object = pickle.load(open('/users/masif/data/masif/ChromAge/encode_histone_data/human/tissue/H3K4me3/processed_data/H3K4me3_mean_bins.pkl', 'rb'))
-    main(H3K4me3_data_object, "H3K4me3")
+    # main(H3K4me3_data_object, "H3K4me3")
 
     # post-processing
-    # main(H3K4me3_data_object, "H3K4me3", True)
+    main(H3K4me3_data_object, "H3K4me3", True)
