f32f8da088a35225e168a1092c381538b45edf6d
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-02-07 18:24:08 -0500

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -311,7 +311,7 @@ def k_cross_validate_model(metadata, histone_data_object, y_test, batch_size, ep
             verbose = 0
         )
 
-        model = create_nn(latent_size, model_params[0], model_params[1], 0.1, model_params[3])
+        model = create_nn(latent_size, model_params[0], model_params[1], model_params[2], model_params[3])
         history = model.fit(auto_encoder.encoder(np.array(training_x)), np.array(training_y), batch_size, epochs, verbose=0, validation_data=(auto_encoder.encoder(np.array(validation_x)), np.array(validation_y)))
         min_train_loss_array.append(np.min(history.history['loss']))
         min_train_mse_array.append(np.min(history.history['mse']))
@@ -321,7 +321,7 @@ def k_cross_validate_model(metadata, histone_data_object, y_test, batch_size, ep
         min_val_mae_array.append(np.min(history.history['val_mae']))
 
         results = model.evaluate(auto_encoder.encoder(np.array(validation_x)), np.array(validation_y), int(batch_size/2))
-        # print("Validation metrics:", results)     
+        print("Validation metrics:", results)     
         val_metrics_array.append(results)
 
         prediction_distribution = model(auto_encoder.encoder(np.array(validation_x)))
