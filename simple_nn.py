e95d379d3420180079358a08b4dd0ca82ce89d29
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-03-13 00:00:38 -0500

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -569,13 +569,13 @@ def post_process(metadata, histone_data_object, histone_mark_str, X_train, X_tes
     history = auto_encoder.fit(
         X_train, 
         y_train, 
-        epochs=200, 
+        epochs=400, 
         batch_size=16, 
         validation_data=(X_test, y_test)
     )
 
-    model = create_nn(50, 3, 0.0003, 0.01, 0.02)
-    history = model.fit(auto_encoder.encoder(np.array(X_train)),np.array(y_train), epochs = 1000, batch_size=16)
+    model = create_nn(50, 3, 0.0003, 0.05, 0.02)
+    history = model.fit(auto_encoder.encoder(np.array(X_train)),np.array(y_train), epochs = 500, batch_size=16)
     print("Model: ", "simple_nn 16 3, 0.0003, 0.0, 0.01", "with min loss, mse, mae: ", [np.min(history.history['loss']), np.min(history.history['mse']), np.min(history.history['mae'])])
     results = model.evaluate(auto_encoder.encoder(np.array(X_test)), np.array(y_test), 16)
     prediction_distribution = model(auto_encoder.encoder(np.array(X_test)))
