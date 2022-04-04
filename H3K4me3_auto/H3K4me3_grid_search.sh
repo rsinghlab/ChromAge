57d6ed643613dfdd2ef1a6dd71db357766eaad57
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-04-04 10:53:00 -0400

    add

---
+++ H3K4me3_auto/H3K4me3_grid_search.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -n 16
+#SBATCH --mem=45G
+#SBATCH -t 500:00:00
+#SBATCH -o H3K4me3-search.out
+#SBATCH -A cbc-condo
+
+module load python/3.7.4
+source ChromAge_venv/bin/activate
+
+python3 /users/masif/data/masif/ChromAge/simple_nn.py
+
+deactivate
