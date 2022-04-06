71f63a07e1ed74ca1d3e220df04c860f3e030e83
Muhammad Haider Asif <masif@login006.oscar.ccv.brown.edu>
2022-04-06 16:54:02 -0400

    add

--- sh_utils/transfer_bigwigs.sh
+++ sh_utils/transfer_bigwigs.sh
@@ -3,13 +3,13 @@
 cd /gpfs/data/rsingh47/masif/caper_output/bigWigs/ 
 for e in *.fc.signal.bigwig ; do
 echo $e
-mv $e /gpfs/data/rsingh47/masif/ChromAge/GEO_histone_data/H3K27ac/raw_data/fc
+mv $e /gpfs/data/rsingh47/masif/ChromAge/GEO_histone_data/H3K4me1/raw_data/fc
 done
 
 cd /gpfs/data/rsingh47/masif/caper_output/bigWigs/
 for e in *.pval.signal.bigwig ; do
 echo $e
-mv $e /gpfs/data/rsingh47/masif/ChromAge/GEO_histone_data/H3K27ac/raw_data/pval
+mv $e /gpfs/data/rsingh47/masif/ChromAge/GEO_histone_data/H3K4me1/raw_data/pval
 done
 
-mv /gpfs/data/rsingh47/masif/caper_output/qcReports/ /gpfs/data/rsingh47/masif/ChromAge/GEO_histone_data/H3K27ac/
+mv /gpfs/data/rsingh47/masif/caper_output/qcReports/ /gpfs/data/rsingh47/masif/ChromAge/GEO_histone_data/H3K4me1/
