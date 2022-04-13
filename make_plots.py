b2f5e15f62f411467e409ca140fa610c2554e994
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2022-04-13 08:31:22 -0400

    Update make_plots.py

--- make_plots.py
+++ make_plots.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
+
 def scatter_plot(ModelArr, ENArr):
     actual_age = []
     predicted_age = []
@@ -26,12 +28,41 @@ def scatter_plot(ModelArr, ENArr):
     ax.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.7)
     
     plt.show()
+    return
 
-scatter_plot(
-    ["H3K4me1_results.csv", "H3K36me3_results.csv",
+def histogram():
+    barWidth = 0.2
+    nn_mae = [4.28, 7.76, 3.64, 3.46, 3.83, 4.36]
+    en_mae = [7.04, 9.64, 7.37, 7.47, 7.00, 7.42]
+
+    r1 = np.arange(len(nn_mae))
+    r2 = [x + barWidth for x in r1] 
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    ax.bar(r1, nn_mae, width = barWidth, color = 'yellow', alpha=0.6, edgecolor = 'black', capsize=7, label='Neural Network')
+    ax.bar(r2, en_mae, width = barWidth, color = 'lightgreen', alpha=1, edgecolor = 'black', capsize=7, label='ElasticNet') 
+
+    plt.xticks([r + barWidth for r in range(len(nn_mae))], ['H3K4me3', 'H3K27ac', 'H3K27me3', 'H3K36me3', 'H3K4me1', 'H3K9me3'])
+    ax.set_ylabel('Test Median Absolute Error (MAE)')
+    ax.set_xlabel("Histone Marks")
+    ax.set_title("NN vs. ElasticNet Test MAE")
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.legend()
+    ax.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.7)
+    plt.show()
+
+    return
+
+ModelCsvArr = ["H3K4me1_results.csv", "H3K36me3_results.csv",
      "H3K27me3_results.csv", "H3K27ac_results.csv",
-      "H3K9me3_results.csv", "H3K4me3_results.csv"],
-      ["ElasticNet-H3K4me1_results.csv", "ElasticNet-H3K36me3_results.csv",
+      "H3K9me3_results.csv", "H3K4me3_results.csv"]
+
+ElasticNetCsvArr = ["ElasticNet-H3K4me1_results.csv", "ElasticNet-H3K36me3_results.csv",
      "ElasticNet-H3K27me3_results.csv", "ElasticNet-H3K27ac_results.csv",
-      "ElasticNet-H3K9me3_results.csv", "ElasticNet-H3K4me3_results.csv"])
+      "ElasticNet-H3K9me3_results.csv", "ElasticNet-H3K4me3_results.csv"]
+
+# scatter_plot(ModelCsvArr,ElasticNetCsvArr)
+histogram()
     
